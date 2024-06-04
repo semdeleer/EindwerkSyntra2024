4431ffb53c96e6c8e2469612624364be8fd2d31b
MrGangrene <stephenvc@protonmail.com>
2024-06-04 12:44:20 +0200

    updated settings to connect to postgresql

--- Django/ecommerce/settings.py
+++ Django/ecommerce/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Django-syntra",
+        "USER": "postgres",
+        "PASSWORD": " ",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
